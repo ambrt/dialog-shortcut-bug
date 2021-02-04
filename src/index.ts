1765e572411e9c4a3f58b3b3bcc7bdf91287eda2
ambrt <ambrth@gmail.com>
2021-02-04 20:45:15 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,37 @@
 import joplin from 'api';
+import { MenuItemLocation, ToolbarButtonLocation } from 'api/types';
+
 
 joplin.plugins.register({
 	onStart: async function() {
 		const dialogs = joplin.views.dialogs;
-
-		const handle = await dialogs.create('myDialog1');
-		await dialogs.setHtml(handle, '<p>Testing dialog with default buttons</p><p>Second line</p><p>Third line</p>');
-		const result = await dialogs.open(handle);
-		alert('Got result: ' + JSON.stringify(result));
-
-		const handle2 = await dialogs.create('myDialog2');
-		await dialogs.setHtml(handle2, '<p>Testing dialog with custom buttons</p><p>Second line</p><p>Third line</p>');
-		await dialogs.setButtons(handle2, [
-			{
-				id: 'ok',
-			},
-			{
-				id: 'cancel',
-			},
-			{
-				id: 'moreInfo',
-				title: 'More info',
-			},
-		]);
-
-		const result2 = await dialogs.open(handle2);
-		alert('Got result: ' + JSON.stringify(result2));
-
 		const handle3 = await dialogs.create('myDialog3');
-		await dialogs.setHtml(handle3, `
-		<p>Testing dialog with form elements</p>
-		<form name="user">
-			Name: <input type="text" name="name" />
-			<br/>
-			Email: <input type="text" name="email"/>
-		</form>
-		`);
+		
+		await joplin.commands.register({
+			name: 'openDialogBug',
+			label: 'Opens Dialog',
+			iconName: 'fas fa-file-import',
+			execute: async () => {
+			
+				
+				await dialogs.setHtml(handle3, `
+				<p>Testing dialog with form elements</p>
+				<form name="user">
+					Name: <input type="text" name="name" />
+					<br/>
+					Email: <input type="text" name="email"/>
+				</form>
+				`);
+				
+		
+				const result3 = await dialogs.open(handle3);
+		
+			}
+		})
 
-		const result3 = await dialogs.open(handle3);
-		alert('Got result: ' + JSON.stringify(result3));		
+		await joplin.views.menuItems.create('openDialogBug', 'openDialogBug', MenuItemLocation.EditorContextMenu, { accelerator: "Ctrl+Alt+N" });
+
+		
 	},
 
 });
